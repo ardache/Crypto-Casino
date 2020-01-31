984f048386a160a20e5660621c264f9d59e4547e
Ardash <ardache@hotmail.com>
2020-01-30 19:39:21 -0600

    haroku issues mongo

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
 mongoose
-  .connect('mongodb+srv://casino:cryptocasino@cluster0-w3bno.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+  .connect('mongodb+srv://casino:cryptocasino@cluster0-w3bno.mongodb.net/test', {useNewUrlParser: true, useUnifiedTopology: true})
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
 
