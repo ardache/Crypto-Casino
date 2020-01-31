aba5da40300fcc4c067cad6940daa3bc0fae311e
Ardash <ardache@hotmail.com>
2020-01-30 19:41:19 -0600

    haroku issues del demonio

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const User = require("./models/user");
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
-mongoose
-  .connect('mongodb+srv://casino:cryptocasino@cluster0-w3bno.mongodb.net/test', {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+// mongoose
+//   .connect('mongodb+srv://casino:cryptocasino@cluster0-w3bno.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+//   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+//   .catch(err => console.error('Error connecting to mongo', err));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
